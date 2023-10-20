dafe3595aa5fe8b03b7e2ae18fbd1025470ed6b7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-20 11:37:20 +0000

    add banners

--- bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-10 max-w-screen-xl bg-black text-white opacity-80">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
