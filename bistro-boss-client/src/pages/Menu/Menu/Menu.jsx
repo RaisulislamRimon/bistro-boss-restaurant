f476b26250fab9be5eace866c14de78cdebd275b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-22 12:16:32 +0000

    Parallax Using React Parallax

--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,16 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
-        <link rel="canonical" href="https://www.tacobell.com/" />
       </Helmet>
-      <h1>Menu</h1>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopularMenu />
     </div>
   );
 };
