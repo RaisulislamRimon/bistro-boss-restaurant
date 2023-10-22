c39ff1d8f67d05df6eb7d6c8dea4b9f4dc71873c
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-22 04:59:47 +0000

    add section title

---
+++ bistro-boss-client/src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,8 @@
+export default function SectionTitle({ heading, subHeading }) {
+  return (
+    <div className="md:w-4/12 mx-auto text-center my-8">
+      <p className="text-yellow-600 mb-2">{subHeading}</p>
+      <h3 className="text-3xl uppercase border-y-4 p-4">{heading}</h3>
+    </div>
+  );
+}
