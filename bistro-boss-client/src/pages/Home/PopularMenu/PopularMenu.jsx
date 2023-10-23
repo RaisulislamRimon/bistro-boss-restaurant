1966a91140d62d7ea8e4ebfab006252580442728
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-23 10:13:08 +0000

    create custom hooks to load data

--- bistro-boss-client/src/pages/Home/PopularMenu/PopularMenu.jsx
+++ bistro-boss-client/src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,26 +1,19 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-  console.log("PopularMenu:", menu.length);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
   return (
     <section>
       <>
         <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"} />
         <div className="grid md:grid-cols-2 gap-10">
-          {menu.map((item) => (
+          {/* {menu.map((item) => ( */}
+          {popular.map((item) => (
             <MenuItem key={item?._id} item={item} />
           ))}
         </div>
--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -4,13 +4,14 @@ import menuImg from "../../../assets/menu/banner3.jpg";
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
+  
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopularMenu />
+      {/* <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopularMenu /> */}
     </div>
   );
 };
