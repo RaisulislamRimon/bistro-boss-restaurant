9149f6a3bad216dd334df823d24df95608b06b88
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-24 15:43:54 +0000

    update

--- bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
+++ bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,13 +1,30 @@
 import FoodCard from "../../../components/FoodCard/FoodCard";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+const pagination = {
+  clickable: true,
+  renderBullet: function (index, className) {
+    return '<span class="' + className + '">' + (index + 1) + "</span>";
+  },
+};
 const OrderTab = ({ items }) => {
   return (
     <div>
-      <div className="grid md:grid-cols-3 gap-10">
-        {items.map((item) => (
-          <FoodCard key={item?._id} item={item} />
-        ))}
-      </div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {items.map((item) => (
+              <FoodCard key={item?._id} item={item} />
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
