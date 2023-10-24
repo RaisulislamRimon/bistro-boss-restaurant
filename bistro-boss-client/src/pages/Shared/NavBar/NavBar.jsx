400d77991c44a8e18564f612724befbb135355bb
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-10-24 15:57:46 +0000

    login page navbar footer hide

--- bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -29,6 +29,9 @@ const NavBar = () => {
       <li>
         <Link to={"/order/salad"}>Order</Link>
       </li>
+      <li>
+        <Link to={"/login"}>Login</Link>
+      </li>
     </>
   );
   return (
